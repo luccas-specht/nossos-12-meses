d871ac43955b389d2968d2e86be00b5e184b7f29
luccas-specht <luccasspecht70@gmail.com>
2024-11-28 22:56:36 -0300

    test icon

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ const observerSection = new IntersectionObserver((entries) => {
 
 const elements = document.querySelectorAll('.hidden');
 elements.forEach((element) => observerSection.observe(element));
+
+let isPlaying = false;
+let hasUserInteractedWith = false;
+const music = document.getElementById('background-music');
+
+window.addEventListener('click', () => {
+  hasUserInteractedWith = true;
+});
+
+window.addEventListener('scroll', () => {
+  if (hasUserInteractedWith && !isPlaying) {
+    music.play();
+    isPlaying = true;
+    console.log('Música tocando');
+  }
+});
